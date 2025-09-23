ef041b323717ca2110bb5c099a4cf2faf374aa41
BakaRickyClariS <ricky860526@gmail.com>
2025-09-23 22:24:12 +0800

    update: add layout path and route

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,13 +2,15 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './components/Home/index.jsx'
 import Login from './components/Login/index.jsx'
 import Signup from './components/Signup/index.jsx'
+import Layout from './Layout.jsx'
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
-      element: <Home />,
+      element: <Layout />,
       children: [
+        { index: true, element: <Home /> }
         { path: 'login', element: <Login /> },
         { path: 'signup', element: <Signup /> },
       ],
