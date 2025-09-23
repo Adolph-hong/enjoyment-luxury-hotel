7f2f613d1ed7dab2d8b7a11e27fc5a2f78c66d38
BakaRickyClariS <ricky860526@gmail.com>
2025-09-23 17:36:19 +0800

    feat: init hero section

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+  return (
+    <section>
+      <div className="w-full bg-cover bg-center bg-[url('/src/assets/home/home-hero-bg.png')]">
+        <div className="">
+          <h2>享樂酒店</h2>
+          <h2>Enjoyment Luxury Hotel</h2>
+          <hr />
+        </div>
+        <div>
+          <h1>高雄</h1>
+          <h1>豪華住宿之選</h1>
+          <p>我們致力於為您提供無與倫比的奢華體驗與優質服務</p>
+          <button>
+            立即訂房
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
