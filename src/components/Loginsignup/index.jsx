fe9f3f94db6e98794a6bca4fa9dfc21e978506e4
Adolph-hong <zxcv010806056@gmail.com>
2025-09-24 12:52:35 +0800

    update: init pages

---
+++ src/components/Loginsignup/index.jsx
@@ -0,0 +1,20 @@
+export { Login, Signup };
+import Layout from "./Layout";
+
+
+const Login = () => {
+    return ( 
+        <Layout>
+            
+        </Layout>
+     );
+}
+
+
+const Signup = () => {
+    return ( 
+        <Layout>
+            
+        </Layout>
+     );
+}
