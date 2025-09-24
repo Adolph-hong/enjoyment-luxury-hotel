1df3847d7b9cc403c4c84d19d67808b851ffb070
iamlouxu <wayne199939@gmail.com>
2025-09-24 17:32:30 +0800

    update: change router

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
-import Header from './components/Header/Header'
+import Header from './components/Header'
 const Layout = () => {
   return (
     <div>
