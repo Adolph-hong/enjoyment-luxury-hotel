f61a1352657b8e1d7ff4c485ee1181855e6b2557
iamlouxu <wayne199939@gmail.com>
2025-09-25 15:42:15 +0800

    update: chaged folder name

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/footer'
-import Header from './components/header'
+import Header from './components/headerr'
 const Layout = () => {
   return (
     <div>
