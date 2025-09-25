8e46a933fbed7038f4fda7e70df042a2968fcdc2
iamlouxu <wayne199939@gmail.com>
2025-09-25 15:48:44 +0800

    update: layout header path

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/footer'
-import Header from './components/headerr'
+import Header from './components/header'
 const Layout = () => {
   return (
     <div>
