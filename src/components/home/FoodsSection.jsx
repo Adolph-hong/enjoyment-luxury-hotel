d7886809710e8be9ff0e4558ee6e27b5ddf7ff7e
BakaRickyClariS <ricky860526@gmail.com>
2025-09-28 14:21:16 +0800

    update: styled foods section

--- src/components/home/FoodsSection.jsx
+++ src/components/home/FoodsSection.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import foodsDeco from '../../assets/home/decos/foods.png'
 
 const FoodsSection = () => {
   const [foods, setFoods] = useState([])
@@ -18,32 +19,44 @@ const FoodsSection = () => {
   }, [])
 
   return (
-    <section>
-      <div className="w-full h-screen bg-[#F7F2EE] py-20 pl-40">
-        <div className="flex flex-row gap-8 items-center">
-          <h2 className="text-3xl font-bold text-[#BF9D7D]">
-            佳餚
-            <br />
-            美饌
-          </h2>
-          <div className="h-[2px] w-24 bg-gradient-to-r from-[#c0a375] to-transparent"></div>
+    <section className="w-full h-screen bg-[#F7F2EE]">
+      <div className="flex flex-row pl-5">
+        <img
+          className="object-cover w-[140px] h-auto pt-10"
+          src={foodsDeco}
+          alt="foodsDeco"
+        />
+        <div className="w-full py-20  pl-20">
+          <div className="flex flex-row gap-8 items-center pb-15">
+            <h2 className="text-3xl font-bold text-[#BF9D7D]">
+              佳餚
+              <br />
+              美饌
+            </h2>
+            <div className="h-[2px] w-24 bg-gradient-to-r from-[#c0a375] to-transparent"></div>
+          </div>
+          <ul className="flex flex-row gap-8 w-full h-auto">
+            {foods.map((foodItem) => (
+              <li
+                key={foodItem._id}
+                className="relative flex flex-col w-full gap-4"
+              >
+                <img
+                  src={foodItem.image}
+                  alt={foodItem.title}
+                  className="absolute inset-0 w-[400px] h-[600px] object-cover"
+                />
+                <div className="z-10 bg-transparent bg-opacity-40 backdrop-blur-md text-white">
+                  <div className="flex flex-row gap-2">
+                    <h3 className="text-2xl font-bold">{foodItem.title}</h3>
+                    <p>{foodItem.diningTime}</p>
+                  </div>
+                  <p className="z-10">{foodItem.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="flex flex-row gap-8">
-          {foods.map((foodItem) => (
-            <li key={foodItem._id} className="relative flex flex-col gap-4">
-              <img
-                src={foodItem.image}
-                alt={foodItem.title}
-                className="absolute inset-0 w-[400px] h-full object-cover"
-              />
-              <div className="flex flex-row z-10 gap-2">
-                <h3 className="text-2xl font-bold">{foodItem.title}</h3>
-                <p>{foodItem.diningTime}</p>
-              </div>
-              <p className="z-10">{foodItem.description}</p>
-            </li>
-          ))}
-        </ul>
       </div>
     </section>
   )
