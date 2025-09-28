84c02d6d720913506bb04215c2ee8f28a63f2a27
BakaRickyClariS <ricky860526@gmail.com>
2025-09-28 23:37:31 +0800

    update: add rwd style

--- src/components/home/FoodsSection.jsx
+++ src/components/home/FoodsSection.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import foodsDeco from '../../assets/home/decos/foods.png'
+import FoodsSwiper from '../swipers/FoodsSwiper'
 
 const FoodsSection = () => {
   const [foods, setFoods] = useState([])
@@ -20,42 +21,24 @@ const FoodsSection = () => {
 
   return (
     <section className="w-full h-screen bg-[#F7F2EE]">
-      <div className="flex flex-row pl-5">
+      <div className="flex flex-row md:pl-5">
         <img
-          className="object-cover w-[140px] h-auto pt-10"
+          className="object-fill w-[150px] h-[110vh] pt-10 hidden md:block"
           src={foodsDeco}
           alt="foodsDeco"
         />
-        <div className="w-full py-20  pl-20">
+        <div className="w-full py-20 px-5 md:pl-20">
           <div className="flex flex-row gap-8 items-center pb-15">
             <h2 className="text-3xl font-bold text-[#BF9D7D]">
               佳餚
               <br />
               美饌
             </h2>
-            <div className="h-[2px] w-24 bg-gradient-to-r from-[#c0a375] to-transparent"></div>
+            <div className="h-[2px] w-[60vw] md:w-24 bg-gradient-to-r from-[#c0a375] to-transparent"></div>
           </div>
-          <ul className="flex flex-row gap-8 w-full h-auto">
-            {foods.map((foodItem) => (
-              <li
-                key={foodItem._id}
-                className="relative flex flex-col w-full gap-4"
-              >
-                <img
-                  src={foodItem.image}
-                  alt={foodItem.title}
-                  className="absolute inset-0 w-[400px] h-[600px] object-cover"
-                />
-                <div className="z-10 bg-transparent bg-opacity-40 backdrop-blur-md text-white">
-                  <div className="flex flex-row gap-2">
-                    <h3 className="text-2xl font-bold">{foodItem.title}</h3>
-                    <p>{foodItem.diningTime}</p>
-                  </div>
-                  <p className="z-10">{foodItem.description}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div className="flex flex-row gap-8 w-full h-auto">
+            <FoodsSwiper foods={foods} />
+          </div>
         </div>
       </div>
     </section>
