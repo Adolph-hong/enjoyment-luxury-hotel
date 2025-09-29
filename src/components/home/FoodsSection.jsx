bc7a3803fb8ae954e6a0cb47509caaddad4c70a0
BakaRickyClariS <ricky860526@gmail.com>
2025-09-29 16:35:11 +0800

    update: fixed rwd problem

--- src/components/home/FoodsSection.jsx
+++ src/components/home/FoodsSection.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import foodsDeco from '../../assets/home/decos/foods.png'
+import { foodsData, baseUrl } from './data'
 import FoodsSwiper from '../swipers/FoodsSwiper'
 
 const FoodsSection = () => {
   const [foods, setFoods] = useState([])
-  const baseUrl = import.meta.env.VITE_API_BASE
+
   useEffect(() => {
     const getLatestFoods = async () => {
       try {
@@ -20,19 +20,18 @@ const FoodsSection = () => {
   }, [])
 
   return (
-    <section className="w-full h-screen bg-[#F7F2EE]">
+    <section className="w-full h-screen bg-[#F7F2EE] overflow-hidden">
       <div className="flex flex-row md:pl-5">
         <img
           className="object-fill w-[150px] h-[110vh] pt-10 hidden md:block"
-          src={foodsDeco}
-          alt="foodsDeco"
+          {...foodsData.decosImg}
         />
         <div className="w-full py-20 px-5 md:pl-20">
           <div className="flex flex-row gap-8 items-center pb-15">
             <h2 className="text-3xl font-bold text-[#BF9D7D]">
-              佳餚
+              {foodsData.title[0]}
               <br />
-              美饌
+              {foodsData.title[1]}
             </h2>
             <div className="h-[2px] w-[60vw] md:w-24 bg-gradient-to-r from-[#c0a375] to-transparent"></div>
           </div>
