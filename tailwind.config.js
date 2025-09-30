194381aeb86d37ef35ee4beff08a57900e5b3d37
BakaRickyClariS <ricky860526@gmail.com>
2025-09-30 13:18:30 +0800

    update: add var into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {
       fontFamily: {
-        brand: ['"Noto Serif TC"', 'serif'],
+        brand: 'var(--font-family-brand)',
+      },
+      backgroundImage: {
+        'hero': 'var(--bg-image-hero)',
+        'about': 'var(--bg-image-about)',
+        'auth': 'var(--bg-image-auth)',
+        'rooms-deco': 'var(--bg-image-rooms-deco)',
       },
     },
   },
