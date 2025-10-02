d1734f15424935a64b6bbc1396b97cbe63434dab
BakaRickyClariS <ricky860526@gmail.com>
2025-10-02 12:48:58 +0800

    update: change hero section into children mode

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,21 +1,10 @@
+import HeroTitle from '../shared/HeroTitle'
 import { heroData } from './data'
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="relative flex flex-col justify-center items-center xl:flex-row w-full h-[120vh] xl:h-screen bg-[image:var(--bg-image-hero)] bg-cover bg-center">
-        <div className="absolute inset-0 bg-black/50"></div>
-        <div className="absolute top-35 xl:relative xl:top-0 xl:w-1/2 xl:gap-0 gap-15 z-10 flex flex-col w-full items-center xl:justify-center xl:items-start text-white xl:pl-20 xl:pr-40 xl:mr-[-50px]">
-          <div className="flex flex-col items-center xl:items-start">
-            <h2 className="text-4xl font-bold text-[#BF9D7D] pb-5">
-              {heroData.title[0]}
-            </h2>
-            <h2 className="text-xl font-bold text-[#BF9D7D]">
-              {heroData.title[1]}
-            </h2>
-          </div>
-          <div className="border-b-2 border-[#BF9D7D] w-[83px] xl:w-full xl:pt-5 rotate-90 xl:rotate-0"></div>
-        </div>
+      <HeroTitle>
         <div className="absolute  inset-0 z-0 top-[42%] xl:top-1/5 left-[15%] xl:left-[52%] ml-[-20px] bg-transparent bg-opacity-40 backdrop-blur-md border-t-1 border-r-1 border-[#F5F7F9] rounded-3xl p-8 w-[85%] xl:w-[47%] h-[500px] text-center"></div>
         <div className="z-10 mt-70 xl:mt-0 w-full pl-5 pr-10 xl:w-[53%] xl:pr-55 flex flex-col justify-center items-center xl:items-start text-white">
           <div>
@@ -31,7 +20,7 @@ const HeroSection = () => {
             <span className="inline-block w-1/5 h-px bg-current"></span>
           </button>
         </div>
-      </div>
+      </HeroTitle>
     </section>
   )
 }
