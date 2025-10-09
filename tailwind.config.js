bb46eb1b375778843b046c781e72cddd72d5884b
BakaRickyClariS <ricky860526@gmail.com>
2025-10-09 17:26:40 +0800

    update: using tailwind config connect index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        brand: 'var(--font-brand)',
+        brand: ['Noto Serif TC', 'sans-serif'],
+      },
+      backgroundImage: {
+        'image-hero': "url('./assets/home/hero.png')",
+        'image-about': "url('./assets/home/about.png')",
+        'image-auth': "url('./assets/auth/bg.jpg')",
+        'image-rooms-deco': "url('./assets/home/decos/rooms-2.png')",
+        'image-account': "url('./assets/account/bg.jpg')",
       },
     },
   },
