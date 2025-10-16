26e1d265753e037edbe06ecf4c6d0248c89a1b6a
BakaRickyClariS <ricky860526@gmail.com>
2025-10-16 23:09:31 +0800

    update: change booking page components into tsx

--- src/components/ui/BlurBg.tsx
+++ src/components/ui/BlurBg.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const BlurBg = ({ children }) => {
+type Props = {
+  children: React.ReactNode
+}
+
+const BlurBg: React.FC<Props> = ({ children }) => {
   return (
     <div className="fixed inset-0 z-40 bg-transparent bg-opacity-40 backdrop-blur-md">
       {children}
